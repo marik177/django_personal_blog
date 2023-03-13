e93c039acbe7338b7893383717e2d080f9e67717
marik177 <marik177@gmail.com>
2023-03-13 15:04:45 +0300

    added Post model

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+
+# admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'publish', 'status']
+    list_filter = ['status', 'created', 'publish', 'author']
+    search_fields = ['title', 'body']
+    raw_id_fields = ['author']
+    date_hierarchy = 'publish'
+    prepopulated_fields = {'slug': ('title',)}
+    ordering = ['status', 'publish']
+
