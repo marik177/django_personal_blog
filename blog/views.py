64a0cc0747b5dacf48deff68a054ca906a644dbf
marik177 <marik177@gmail.com>
2023-03-19 17:02:31 +0300

    full-text search added SearchRank and stop language

--- blog/views.py
+++ blog/views.py
@@ -9,7 +9,8 @@ from django.views.decorators.http import require_POST
 from .models import Post
 from .forms import EmailPostForm, CommentForm, SearchForm
 from taggit.models import Tag
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, \
+    SearchQuery, SearchRank
 
 
 def post_list(request, tag_slug=None):
@@ -112,9 +113,12 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+            search_vector = SearchVector('title', 'body', config='russian')
+            search_query = SearchQuery(query, config='russian')
             results = Post.objects.\
-                annotate(search=SearchVector('title', 'body')).\
-                filter(search=query)
+                annotate(search=search_vector,
+                         rank=SearchRank(search_vector, search_query)).\
+                filter(search=search_query).order_by('-rank')
 
     return render(request, 'blog/post/search.html',
                   {'form': form, 'query': query, 'results': results})
